960b6e2276affd9f3688c1922637e8c02d4013e5
Angel Aguero <angelagueror23@gmail.com>
2025-06-08 00:14:39 -0400

    feat: mejorar la gestión de selección de talla y cantidad en el componente AddToCart

--- app/components/product/AddToCart.tsx
+++ app/components/product/AddToCart.tsx
@@ -11,8 +11,30 @@ interface Props {
 export const AddToCart = ({ availableSizes, size }: Props) => {
   const [selectSize, setSelectSize] = useState<Size | undefined>(size);
   const [selectQuantity, setQuantity] = useState<number>(1);
+  const [posted, setPosted] = useState<boolean>(false);
+  const addToCart = (): void => {
+    setPosted(true);
+    // Aquí puedes implementar la lógica para agregar al carrito
+    if (!selectSize || selectQuantity < 1) {
+      console.error("Debe seleccionar una talla y una cantidad válida.");
+      return;
+    }
+    // Simulación de agregar al carrito
+    console.log("Producto agregado al carrito:", {
+      size: selectSize,
+      quantity: selectQuantity,
+    });
+  };
   return (
     <>
+      {/* Mensaje de debe selecionas un a talla  */}
+      {posted && !selectSize && (
+        <div className="text-red-500 text-sm mb-4">
+          Debe seleccionar una talla antes de agregar al carrito.
+        </div>
+      )}
+      {/* Mensaje de debe selecionas una cantidad  */}
+      
       {/* Selector de Tallas */}
       <div className="mb-8">
         <SizeTab
@@ -28,7 +50,13 @@ export const AddToCart = ({ availableSizes, size }: Props) => {
           onQuantityChange={setQuantity}
         />
       </div>
-      <button className="w-full bg-[#171a20] text-white py-4 rounded-full font-semibold text-lg hover:bg-[#22262b] transition-colors duration-200 shadow-none tracking-widest uppercase mb-10">
+      <button
+        onClick={addToCart}
+        type="button"
+        aria-label="Agregar al carrito"
+        disabled={!selectSize || selectQuantity < 1}
+        className="w-full bg-[#171a20] text-white py-4 rounded-full font-semibold text-lg hover:bg-[#22262b] transition-colors duration-200 shadow-none tracking-widest uppercase mb-10"
+      >
         Agregar al carrito
       </button>
     </>
